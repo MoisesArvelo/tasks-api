345d1711e43fafeb4723d2c25d3a8928262fcd34
Juan Arvelo <jmarvelon@gmail.com>
2024-05-21 02:53:59 -0400

    feat: :sparkles: Authorization for Cognito AuthProvider
    Implemente Passport Strategy with JWT for authorize transactions

--- config/custom-environment-variables.ts
+++ config/custom-environment-variables.ts
@@ -4,4 +4,6 @@ export default {
   port: "PORT",
   pinoLogLevel: "PINO_LOG_LEVEL",
   dbUrl: "DB_URL",
+  awsCognitoClientId: "AWS_COGNITO_CLIENT_ID",
+  awsCognitoIssuer: "AWS_COGNITO_ISSUER",
 };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,18 @@
 import { Response, Router } from "express";
 import { formatResponse } from "../utils/response/Response";
 
-// Routes Stacks
+// Routes Stacks Imports
 import taskRoute from "./task.route";
 
-// Routes Stacks Imports
+import passport from "passport";
+import passportConfig, { authenticate } from "../utils/passport/Passport";
+passportConfig(passport);
+
 const router = Router();
 
 router.get("/", (_res, res: Response) =>
   formatResponse(res, 200, "Hello API", {}),
 );
-router.use("/api/v1/task", taskRoute);
+router.use("/api/v1/task", authenticate(), taskRoute);
 
 export default router;
